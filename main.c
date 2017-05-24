a58ac845ae10fd33d0acffd9589e878149594815
houssana <hamid.oussana@gmail.com>
2017-05-24 16:16:18 +0200

    temp

--- main.c
+++ main.c
@@ -11,17 +11,20 @@
 /* ************************************************************************** */
 
 #include "mlx.h"
+#include "libft/libft.h"
+#include "libft/get_next_line.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 int my_key_funct(int keycode, void *param)
 {
 	printf("key event %d\n", keycode);
-//	mlx_pixel_put(mlx, win, 300, 300, 0xFF0FF00);
+	//	mlx_pixel_put(mlx, win, 300, 300, 0xFF0FF00);
 	return (0);
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
 	void	*mlx;
 	void	*win;
@@ -32,38 +35,106 @@ int main(void)
 	int		*size_line;
 	char	*img_addr;
 	int		*endian;
-	int		i;
+	char	*line;
+	int	i;
+	int	j;
+	int	k;
+	int	fd;
 
-	i = -1;
+	if (argc != 2)
+	{
+		return (0);
+	}
+	i = 0;
+	j = 0;
+	k = 0;
+	fd = open(argv[1], O_RDONLY);
+	while (get_next_line(fd, &line) > 0)
+	{
+		j++;
+		while (line[k] != '\0')
+		{
+			i++;
+			while (line[k] == ' ')
+				k++;
+			k += ft_intlen_base(ft_atoi(line), 10);	
+		}
+		free(line);
+	}
+
+	k = -1;
 	bits_per_pixel = malloc(sizeof(int));
 	size_line = malloc(sizeof(int));
 	endian = malloc(sizeof(int));
 	img_addr = malloc(sizeof(char));
 	mlx = mlx_init();
 	x = 0;
-	win = mlx_new_window(mlx, 400, 400, "mlx 42");
-//	mlx_pixel_put(mlx, win, 200, 200, 0x00FFFFFF);
-	img = mlx_new_image(mlx, 100, 100);
+	win = mlx_new_window(mlx, 10 * i + 100, 10 * j + 100, "mlx 42");
+	//	mlx_pixel_put(mlx, win, 200, 200, 0x00FFFFFF);
+	img = mlx_new_image(mlx, 10 * i + 100, 10 * j + 100);
 	img_addr = mlx_get_data_addr(img, bits_per_pixel, size_line, endian);
-	*(img_addr + 0) = 255;
-	*(img_addr + 1) = 0;
-	*(img_addr + 2) = 0;
-//	while (++i <= 0)
-//	{
-//		*(img_addr + i) = 255;
-		//printf("img : %s\n", img_addr);
-//	}
-	printf("bits_per_pixel : %d\nsize_line : %d\nendian : %d\nimg : %s\n", *bits_per_pixel, *size_line, *endian, (img_addr));
-	mlx_put_image_to_window(mlx, win, img, 250, 250);
-	while (++x < 200)
+	while (i)
 	{
-		y = 0;
-		while (++y <= 200)
-		{
-			mlx_pixel_put(mlx, win, x, y, 0x00FFFFFF);
+		k = j;
+		while (k)
+		{		
+			*(img_addr + 0 + 1*(k * *size_line + i**bits_per_pixel)) = 255;
+			*(img_addr + 1+ 1*(k * *size_line + i**bits_per_pixel)) = 255;
+			*(img_addr + 2+ 1*(k * *size_line + i**bits_per_pixel)) = 255;
+			k--;
 		}
+		i--;
 	}
-	mlx_key_hook(win, my_key_funct, 0);
+	//*(img_addr + 0) = 255;
+	//*(img_addr + 1) = 0;
+	//*(img_addr + 2) = 0;
+	//	while (++i <= 0)
+	//	{
+	//		*(img_addr + i) = 255;
+	//printf("img : %s\n", img_addr);
+	//	}
+	printf("bits_per_pixel : %d\nsize_line : %d\nendian : %d\nimg : %s\n", *bits_per_pixel, *size_line, *endian, (img_addr));
+	mlx_put_image_to_window(mlx, win, img, 0, 0);
+//	while (++x < 200)
+//	{
+//		y = 0;
+//		while (++y <= 200)
+//		{
+//			mlx_pixel_put(mlx, win, x, y, 0x00FFFFFF);
+//		}
+//	}
+//	mlx_key_hook(win, my_key_funct, 0);
 	mlx_loop(mlx);
 	return (0);
 }
+
+//int main(int argc, char **argv)
+//{
+//	char	*line;
+//	int	i;
+//	int	j;
+//	int	k;
+//	int	fd;
+//
+//	if (argc != 2)
+//	{
+//		return (0);
+//	}
+//	i = 0;
+//	j = 0;
+//	k = 0;
+//	fd = open(argv[1], O_RDONLY);
+//	while (get_next_line(fd, &line) > 0)
+//	{
+//		j++;
+//		while (line[k] != '\0')
+//		{
+//			i++;
+//			while (line[k] == ' ')
+//				k++;
+//			k += ft_intlen_base(ft_atoi(line), 10);	
+//		}
+//		free(line);
+//	}
+//	return (0);
+//}
