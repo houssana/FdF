a4a2e7b9712ee56f81744210295f75b77a800d42
Hamid OUSSANA <houssana@e2r9p20.42.fr>
2017-06-03 13:02:02 +0200

    tmp

--- fdf.h
+++ fdf.h
@@ -6,14 +6,34 @@
 /*   By: houssana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/31 13:34:05 by houssana          #+#    #+#             */
-/*   Updated: 2017/05/31 13:39:49 by houssana         ###   ########.fr       */
+/*   Updated: 2017/06/03 12:25:44 by houssana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-char		*ft_strjoin_free(char *s1, char *s2, int w);
-int			**parse(char *str);
-char		*to_str(char *f);
+char			*ft_strjoin_free(char *s1, char *s2, int w);
+int				**parse(char *str);
+char			*to_str(char *f);
+typedef struct	s_point2
+{
+	int x;
+	int y;
+}				t_p2;
+typedef struct	s_point3
+{
+	int x;
+	int y;
+	int z;
+}				t_p3;
+typedef struct	s_img
+{
+	void	*img;
+	char	*img_addr;
+	int		*bits_per_pixel;
+	int		*size_line;
+	int		*endian;
+	int		scale;
+}				t_img;
 #endif
